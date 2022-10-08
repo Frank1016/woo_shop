fefb1905d9fc4017a8fc0d3220a5444c1a7a921b
Frank <frank@Fanjiangs-MacBook-Air.local>
2022-10-08 19:27:08 -0400

    splash countdown added

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,8 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:woo_shop/common/app_colors.dart';
+import 'package:woo_shop/common/assets.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  //counting var
+  final duration = 10;
+  int number = 0;
+  //counting function
+  Future<void> _countdown() async {
+    number = duration;
+    for (var i = 0; i < duration; i++) {
+      await Future.delayed(const Duration(seconds: 1), () {
+        if (mounted == true) {
+          setState(() {
+            number--;
+          });
+        }
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _countdown();
+  }
+
+//logo
+  Widget _buildLogo() {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        //background
+        Container(
+          width: 120,
+          height: 120,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(120 / 2)),
+        ),
+        //logo pic
+        Image.asset(
+          AssetsImages.logoPng,
+          width: 84,
+          height: 80,
+        ),
+      ],
+    );
+  }
+
+//Text
+  Text _buildText(String text) {
+    return Text(
+      text,
+      style: const TextStyle(
+          fontSize: 19,
+          fontFamily: "Poppins",
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          height: 22 / 19),
+    );
+  }
 
   //main view
   Widget _buildView(BuildContext context) {
@@ -11,15 +76,18 @@ class SplashPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           //logo
-          Container(
-            color: Colors.white,
-            width: 120,
-            height: 120,
+          _buildLogo(),
+          const SizedBox(
+            height: 24,
           ),
           //title
-          const Text('Online Market'),
+          _buildText('Online Market'),
+          const SizedBox(
+            height: 27,
+          ),
           //counter
-          const Text('10'),
+          _buildText(number > 0 ? '$number' : 'done'),
+
           //end
         ],
       ),
